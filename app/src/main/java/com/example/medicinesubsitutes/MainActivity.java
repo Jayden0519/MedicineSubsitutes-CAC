fd390e205b6d564e21da58e7d5eb76439c398b30
91094638 <jayden.jimenez2005@gmail.com>
2022-10-21 12:15:00 -0700

    finished completing the first page of my competition website about medicinal subs.

---
+++ app/src/main/java/com/example/medicinesubsitutes/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.medicinesubsitutes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void next(View v)
+    {
+        Log.i("click", "you clicked button next");
+        Intent intent = new Intent(MainActivity.this, context.class);
+        startActivity(intent);
+        Toast.makeText(this, "you clicked next!", Toast.LENGTH_LONG).show();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/medicinesubsitutes/context.java
@@ -0,0 +1,5 @@
+package com.example.medicinesubsitutes;
+
+public class context {
+
+}
