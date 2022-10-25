384dd0430c9af0358632c372a109c6e305d78f35
91094638 <jayden.jimenez2005@gmail.com>
2022-10-25 12:11:51 -0700

    me and janeth were struggling because Android Studio is still extremely new to me so I'm making due the very best I am but im learning so much in the process.

--- app/src/main/java/com/example/medicinesubsitutes/MainActivity.java
+++ app/src/main/java/com/example/medicinesubsitutes/MainActivity.java
@@ -6,15 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button nextBtn;
+    private Button nextBtn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextBtn = (Button) findViewById(R.id.startButton);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextpage();
+            }
+        });
+
     }
+
+    public void nextpage()
+    {
+        Intent intent = new Intent(this, context.class);
+        startActivity(intent);
+    }
+
+
 
     public void next(View v)
     {
@@ -23,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         Toast.makeText(this, "you clicked next!", Toast.LENGTH_LONG).show();
     }
+
+
 
 
 }
--- app/src/main/java/com/example/medicinesubsitutes/context.java
+++ app/src/main/java/com/example/medicinesubsitutes/context.java
@@ -1,5 +1,16 @@
 package com.example.medicinesubsitutes;
 
-public class context {
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class context extends AppCompatActivity {
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
 }
