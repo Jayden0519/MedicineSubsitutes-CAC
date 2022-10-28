ad147b17736a58feacff347614c51108fca41912
91094638 <jayden.jimenez2005@gmail.com>
2022-10-28 12:08:19 -0700

    This is the FIRST completed version if I do have time to add to the website I will but as of now this is the completed version i will check for any extra spelling or overall errors IF I CAN GET ANDROID STUDIO TO WORK AT HOME!!!!. Im gonna watch a youtube video for some help :(

--- app/src/main/java/com/example/medicinesubsitutes/solutions.java
+++ app/src/main/java/com/example/medicinesubsitutes/solutions.java
@@ -1,18 +1,36 @@
 package com.example.medicinesubsitutes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class solutions extends AppCompatActivity {
 
+    private Button nextBtn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        nextBtn2 = (Button) findViewById(R.id.button3);
+        nextBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextpage3();
+            }
+        });
+
     }
-
-
+    public void nextpage3()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
-
